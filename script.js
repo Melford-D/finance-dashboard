8b085b63ffca403600b82ebeca55bd50e9369ab2
huzitaiwo <taiwohusayn5@gmail.com>
2021-11-17 09:13:26 +0100

    completed webpage reponsiveness

---
+++ script.js
@@ -0,0 +1,15 @@
+const sidebar = document.querySelector('.sidebar');
+const menuBtn = document.querySelector('.hamburger');
+const menuIcon = document.querySelector('.hamburger img');
+const mainPage = document.querySelector('main');
+
+menuBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+
+    // toggle close icon
+    if(sidebar.classList.contains('active')) {
+        menuIcon.src = './icons/close.svg';
+    } else {
+        menuIcon.src = './icons/hamburger.svg';
+    }
+})
